ac38307a7ea9f2b7bf0ea977e7414dc1f2a61df9
unknown <Rajesh@.(none)>
2013-02-15 22:48:52 +0530

    ledger

--- app/controllers/ledgers_controller.rb
+++ app/controllers/ledgers_controller.rb
@@ -23,7 +23,7 @@ add_breadcrumb "index", ledgers_path
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @ledgers }
+      format.json { render :json => @ledgers }
     end
   end
 
@@ -37,7 +37,7 @@ add_breadcrumb "index", ledgers_path
     
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @ledger }
+      format.json { :json => @ledgers  }
     end
   end
 
@@ -48,7 +48,7 @@ add_breadcrumb "index", ledgers_path
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @ledger }
+      format.json { :json => @ledgers  }
     end
   end
 
